63fc22c82757aa9cfae85a42156f3023be9e8a47
girijajyoti <lkqueengirija@gmail.com>
2024-01-07 20:07:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function prev(){
+    document.getElementById('slider-container').scrollLeft -= 270;
+}
+
+function next()
+{
+    document.getElementById('slider-container').scrollLeft += 270;
+}
+
+
+$(".slide img").on("click" , function(){
+$(this).toggleClass('zoomed');
+$(".overlay").toggleClass('active');
+})
+
+
+
+
+
+
+
+
+
+
+
